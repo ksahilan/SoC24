cb454d26d738bb344cfa0e259d65e6c2587f2eaa
K S Ahilan <142374091+ksahilan@users.noreply.github.com>
2024-06-09 19:05:40 +0530

    Update and rename Week2_Q5.cpp to 5_SingleNumber.cpp

--- Week2/5_SingleNumber.cpp
+++ Week2/5_SingleNumber.cpp
@@ -3,8 +3,10 @@ public:
     int singleNumber(vector<int>& nums) {
         int dis = 0;
         for(auto x : nums){                    // for loop
-            dis = dis^x
+            dis = dis^x                        // A^B^C^B^A^D^E^E^D = C : a property of XOR
         }
         return dis;
     }
 };
+
+//https://leetcode.com/problems/single-number/description/?envType=study-plan-v2
